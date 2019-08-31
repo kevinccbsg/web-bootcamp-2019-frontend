0eee5aed6c847d21499264a3277666fc1d0bf3a4
kevin Martínez <kevinccbsg@gmail.com>
2019-08-31 16:38:44 +0200

    Basic styles

---
+++ src/js/ui.js
@@ -0,0 +1,32 @@
+
+const navbar = document.getElementById('navbar');
+const navbarLogo = document
+  .querySelector('.navbar-logo');
+const searchIcon = document
+  .getElementById('navbar-search');
+const closeIcon = document
+  .getElementById('navbar-close');
+
+const toggle = elemento =>
+  (removeClass, addClass) => {
+    elemento.classList.remove(removeClass);
+    elemento.classList.add(addClass);
+  };
+
+const navbarVariable = toggle(navbar);
+
+searchIcon.addEventListener('click', () => 
+  navbarVariable('no-search', 'search'));
+
+closeIcon.addEventListener('click', () => 
+  navbarVariable('search', 'no-search'));
+
+const openHeader = (id) => (evt) => {
+  console.log(evt);
+  const elemento = document.getElementById(id);
+  elemento.classList.toggle('close');
+};
+
+export {
+  openHeader,
+};
