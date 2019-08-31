da78c84d34fb99465d92ec647f328a05ae69c23c
kevin Martínez <kevinccbsg@gmail.com>
2019-08-31 18:21:29 +0200

    Tests with page.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
 import './ui.js';
 import './shows.js';
 import './navbar.js';
-
-console.log('Hello world');
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,7 +1,18 @@
+
+let store = {
+  shows: [],
+};
+
 page('/', () => {
-  console.log('wat');
+  console.log('Load index');
 })
-page('/detail/:id', ctx => {
+page('/detail/:id', (ctx, next) => {
+  const { params } = ctx;
   console.log(ctx);
+  store = { ...store, shows: [1, 2, 3] };
+  next();
+}, ctx => {
+  console.log(store);
+  console.log('next one');
 })
 page();
